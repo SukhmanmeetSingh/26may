5221c645d97cfebc3c4f5a3b628a00ede36cbc40
Sukhmanmeet Singh <sin14089@sheridancollege.ca>
2023-05-26 08:10:46 -0700

    Add day

---
+++ src/arraydemos23friam/Student.java
@@ -0,0 +1,34 @@
+
+package arraydemos23friam;
+
+
+public class Student {
+    //  comment added at 9:27AM
+    //friday
+    private String sId;
+    private String sName;
+
+    public Student(String sId, String sName) {
+        this.sId = sId;
+        this.sName = sName;
+    }   
+    
+    public String getsId() {
+        return sId;
+    }
+
+    public void setsId(String sId) {
+        this.sId = sId;
+    }
+
+    public String getsName() {
+        return sName;
+    }
+
+    public void setsName(String sName) {
+        this.sName = sName;
+    }
+    
+    
+    
+}
