695d31ff87bb2ebb19238a40c4d6cb5afc3edff6
SukhmanmeetSingh <134074138+SukhmanmeetSingh@users.noreply.github.com>
2023-05-26 06:18:47 -0700

    Update ArrayDemoS23FriAM.java

--- src/arraydemos23friam/ArrayDemoS23FriAM.java
+++ src/arraydemos23friam/ArrayDemoS23FriAM.java
@@ -8,6 +8,7 @@ import java.util.Scanner;
 public class ArrayDemoS23FriAM {
 //third comment
     //fourth comment
+    //fifth comment
     public static void main(String[] args) {
        System.out.println("Enter a word");
        Scanner input = new Scanner(System.in);
